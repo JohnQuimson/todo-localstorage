0faeb273a85d0ff3faa34d4377f6035b260ea3ee
JohnQuimson <john.quimson777@gmail.com>
2024-04-23 10:36:18 +0100

    feat: add todo to array in store

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,63 @@
+<template>
+  <div>
+    <h1>Ciao {{ name }}!</h1>
+    <input
+      v-if="editingName"
+      type="text"
+      v-model="inputValue"
+      @input="saveToLocalStorage"
+    />
+    <button @click="toggleEditing">
+      {{ editingName ? 'conferma' : name ? 'cambia nome' : 'inserisci nome' }}
+    </button>
+  </div>
+</template>
+
 <script>
 export default {
   name: 'Header',
   data() {
-    return {};
+    return {
+      name: '',
+      inputValue: '',
+      editingName: false,
+    };
+  },
+  mounted() {
+    const savedValue = localStorage.getItem('inputValue');
+    if (savedValue) {
+      this.inputValue = savedValue;
+    }
+
+    const savedName = localStorage.getItem('name');
+    if (savedName) {
+      this.name = savedName;
+    }
+  },
+  methods: {
+    saveToLocalStorage() {
+      localStorage.setItem('inputValue', this.inputValue);
+    },
+    toggleEditing() {
+      this.editingName = !this.editingName;
+      if (!this.editingName) {
+        this.changeName();
+      }
+    },
+    changeName() {
+      if (this.inputValue.trim() !== '') {
+        this.name = this.inputValue;
+        localStorage.setItem('name', this.name);
+        console.log('Nome salvato nel localStorage:', this.name); // Aggiungi il console.log qui
+        this.inputValue = '';
+      } else {
+        alert('Il nome non può essere vuoto!');
+      }
+    },
   },
 };
 </script>
 
-<template>
-  <p>Funziona tutto !</p>
-</template>
-
 <style scoped lang="scss">
-@use '../assets/scss/style.scss' as *;
+/* Stili CSS qui */
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,58 @@
 <script>
+import { store } from '../store';
+
 export default {
   name: 'Main',
+  data() {
+    return {
+      store,
+      todoInput: '',
+    };
+  },
+
+  methods: {
+    addTodo() {
+      console.log('Metodo addTodo chiamato');
+      if (this.todoInput.trim() != '') {
+        store.todos.push(this.todoInput);
+        console.log('Todo aggiunto:', this.todoInput);
+        this.todoInput = '';
+        console.log('Store:', store.todos);
+
+        // Salva il todo nel local storage dopo averlo aggiunto all'array
+        localStorage.setItem('todos', JSON.stringify(store.todos));
+      } else {
+        alert('inserisci un todo valido');
+      }
+    },
+  },
+
+  created() {
+    // Carica i todos dal localStorage quando il componente viene creato
+    const todosJSON = localStorage.getItem('todos');
+    if (todosJSON) {
+      store.todos = JSON.parse(todosJSON);
+    }
+  },
   components: {},
 };
 </script>
 
-<template></template>
+<template>
+  <h2>crea todo</h2>
+  <form @submit.prevent="addTodo">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">scrivi un to-do</label>
+      <input type="text" class="form-control" v-model="todoInput" />
+    </div>
+
+    <button type="submit" class="btn btn-primary">Aggiungi</button>
+  </form>
+
+  <h3>I tuoi todo:</h3>
+  <ul>
+    <li v-for="todo in store.todos">{{ todo }}</li>
+  </ul>
+</template>
 
 <style lang="scss" scoped></style>
